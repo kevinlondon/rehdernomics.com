2fcc52413574bc2444d1e37b67124224f4462285
Kevin London <kevinlondon@gmail.com>
2013-09-18 22:13:36 -0700

    Move blog to recipe

--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+
+from blog.models import Post
+
 
 def home(request):
-    return render(request, "recipe/home.html")
+    context = {'posts': Post.objects.all()}
+    return render(request, 'home.html', context)
+
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+
+def tagpage(request, tag):
+    posts = Post.objects.filter(tags__name=tag)
+    return render(request, "blog/tagpage.html", {
+        "posts": posts,
+        "tag": tag
+    })
