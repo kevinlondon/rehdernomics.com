42bd025074382e26dc711bbaaf92ecbb46abf2dd
Kevin London <kevinlondon@gmail.com>
2013-11-21 23:37:36 -0800

    Fix password change link.

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}, name="account-login"),
     url(r'^profile/$', 'account.views.profile', name='account-profile'),
     url(r'^change_password/$', 'django.contrib.auth.views.password_change', name='account-password-change'),
-    url(r'^change_password/done/$', 'django.contrib.auth.views.password_change_done', name="account-password-changed"),
+    url(r'^change_password/done/$', 'django.contrib.auth.views.password_change_done', name="password_change_done"),
 )
